8c8d530b77bb9a391d5b5bd57fe762085d36ac88
HemanthiSai <42537854+HemanthiSai@users.noreply.github.com>
2021-05-04 14:54:43 +0530

    Add files via upload

---
+++ myfirstreact/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1>Welcome the first session of React</h1>
+<p>HEllo Everyone</p>
+  );
+}
+
+export default App;
